65f70a54a02de5aecc2408d0fde53b7949ebe922
RexRagnarokk <36676287+RexRagnarokk@users.noreply.github.com>
2018-03-27 19:52:19 +0530

    small fixes

--- rom-build-dependencies.sh
+++ rom-build-dependencies.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 # Ubuntu 16.04 and further
-sudo apt-get install -y repo bc bison git-core gnupg flex bison gperf libsdl1.2-dev libesd0-dev 
-libwxgtk3.0-dev squashfs-tools build-essential zip curl libncurses5-dev zlib1g-dev openjdk-8-jre 
-openjdk-8-jdk pngcrush schedtool libxml2 libxml2-utils xsltproc lzop libc6-dev schedtool
-g++-multilib lib32z1-dev lib32ncurses5-dev lib32readline-gplv2-dev gcc-multilib maven tmux 
-screen w3m ncftp
+
+sudo apt-get update
+sudo apt-get install -y repo bc bison git-core gnupg flex bison gperf libsdl1.2-dev libesd0-dev libwxgtk3.0-dev squashfs-tools build-essential zip curl libncurses5-dev zlib1g-dev openjdk-8-jre openjdk-8-jdk pngcrush schedtool libxml2 libxml2-utils xsltproc lzop libc6-dev schedtoolg++-multilib lib32z1-dev lib32ncurses5-dev lib32readline-gplv2-dev gcc-multilib maven tmux screen w3m ncftp
+sudo apt-get update
