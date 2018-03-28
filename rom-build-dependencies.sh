fa2c98eea77bfed7fcb87169d5f806bc4d2339b1
RexRagnarokk <36676287+RexRagnarokk@users.noreply.github.com>
2018-03-28 22:33:58 +0530

    added support for arch linux

--- rom-build-dependencies.sh
+++ rom-build-dependencies.sh
@@ -1,6 +1,34 @@
 #!/bin/bash
-# Ubuntu 16.04 and further
+# Simple bash script to install dependencies on Arch and Ubuntu distros 
 
-sudo apt-get update
-sudo apt-get install -y repo bc bison git-core gnupg flex bison gperf libsdl1.2-dev libesd0-dev libwxgtk3.0-dev squashfs-tools build-essential zip curl libncurses5-dev zlib1g-dev openjdk-8-jre openjdk-8-jdk pngcrush schedtool libxml2 libxml2-utils xsltproc lzop libc6-dev schedtoolg++-multilib lib32z1-dev lib32ncurses5-dev lib32readline-gplv2-dev gcc-multilib maven tmux screen w3m ncftp
-sudo apt-get update
+printf " *** Installing Dependecies to Build Android Roms ***"
+printf " Choose OS\n 1.Arch Linux\n 2.Ubuntu 16.04\n\n"
+printf "Enter option: "
+read option
+
+# Arch linux 
+function arch { 
+    printf "Installing dependencies on Arch Linux\n\n"
+    sudo pacman -Syyu 
+    sudo pacman -S pacaur 
+    gpg --recv-keys 702353E0F7E48EDB
+    pacaur -S aosp-devel lineageos-devel
+    printf "Remember to have python2 (not python3) in your PATH when building the rom\n"
+    printf "or run \nvirtualenv2 venv \nsource venv/bin/activate \nexport LC_ALL=C\n\n"
+}
+
+# Ubuntu 16.04 and further
+function ubuntu {
+    printf "Installing dependencies on Ubuntu 16.04\n\n"
+    sudo apt-get update
+    sudo apt-get install -y repo bc bison git-core gnupg flex bison gperf libsdl1.2-dev libesd0-dev libwxgtk3.0-dev squashfs-tools build-essential zip curl libncurses5-dev zlib1g-dev openjdk-8-jre openjdk-8-jdk pngcrush schedtool libxml2 libxml2-utils xsltproc lzop libc6-dev schedtoolg++-multilib lib32z1-dev lib32ncurses5-dev lib32readline-gplv2-dev gcc-multilib maven tmux screen w3m ncftp
+    sudo apt-get update
+}
+
+if [ "$option" = "1" ]; then
+    arch
+elif [ "$option" = "2" ];then
+    ubuntu
+else 
+    printf " Invalid Option, run again with valid option\n\n"    
+fi 
